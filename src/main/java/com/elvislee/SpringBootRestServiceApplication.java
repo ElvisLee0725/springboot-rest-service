034c6dbe97fab396563f0a3deef2e2afec20e870
ElvisLee0725 <elvislee0725@gmail.com>
2021-09-29 22:47:33 -0700

    application properties setup for JPA

--- src/main/java/com/elvislee/SpringBootRestServiceApplication.java
+++ src/main/java/com/elvislee/SpringBootRestServiceApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
 public class SpringBootRestServiceApplication implements CommandLineRunner {
 
@@ -15,14 +17,31 @@ public class SpringBootRestServiceApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestServiceApplication.class, args);
-
-
 	}
 
 
 	@Override
 	public void run(String[] args) throws Exception{
 		Library lib = repository.findById("fdsefr343").get();
-//		System.out.println(lib.getAuthor());
+		System.out.println(lib.getAuthor());
+
+		Library libEntity = new Library();
+		libEntity.setAisle(123);
+		libEntity.setAuthor("Elvis Lee");
+		libEntity.setBook_name("Devops");
+		libEntity.setIsbn("dsf45345lds");
+		libEntity.setId("abcde1234");
+
+		// repository.save(libEntity);
+
+		List<Library> allRecords = repository.findAll();
+		for(Library item : allRecords) {
+			System.out.println(item.getAuthor());
+		}
+
+		// Delete an item that matches entity
+		//repository.delete(libEntity);
+
+		// repository.deleteById("abcde1234");
 	}
 }
